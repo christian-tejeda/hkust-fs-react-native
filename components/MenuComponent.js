2fa6e3c2f3286f35fe952bdeaa80f2230f1a1085
Christian <christian.tejeda@fing.edu.uy>
2020-09-14 21:27:17 -0300

    Components pt 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
-function Menu(props) {
+import Dishdetail from './DishdetailComponent';
 
+function Menu(props) {
     const renderMenuItem = ({item, index}) => {
 
         return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    leftAvatar={{ source: require('./images/uthappizza.png')}}
-                  />
+            <ListItem
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron={true}
+                onPress={() => props.onPress(item.id)}
+                leftAvatar={{ source: require('./images/uthappizza.png')}}
+            />
         );
     };
 
